8294bf9c84ecb940f4eeffb3585c2c1db24e1e93
noname_dev <noname_dev@naver.com>
2023-01-08 22:30:09 +0900

    인증 성공 핸들러: CustomAuthenticationSuccessHandler

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.config.annotation.web.configurers.RequestCac
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 //@EnableWebSecurity  //springboot를 쓸때는 자동 설정해줌
@@ -25,6 +26,8 @@ public class SecurityConfig {
 
     @Autowired
     private AuthenticationDetailsSource authenticationDetailsSource;
+    @Autowired
+    private AuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -78,6 +81,7 @@ public class SecurityConfig {
                 .loginProcessingUrl("/login_proc")
                 .defaultSuccessUrl("/")
                 .authenticationDetailsSource(authenticationDetailsSource)
+                .successHandler(customAuthenticationSuccessHandler)
                 .permitAll();
         return http.build();
     }
